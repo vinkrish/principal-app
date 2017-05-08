5373efb5e27c0ee6d23d671552a8add04147b9b9
vinkrish <vinaykrishna1989@gmail.com>
2017-05-08 20:19:55 +0530

    Group message vew

--- app/src/main/java/com/aanglearning/principalapp/messagegroup/MessageActivity.java
+++ app/src/main/java/com/aanglearning/principalapp/messagegroup/MessageActivity.java
@@ -1,23 +1,142 @@
 package com.aanglearning.principalapp.messagegroup;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.aanglearning.principalapp.R;
+import com.aanglearning.principalapp.dao.GroupDao;
+import com.aanglearning.principalapp.model.Groups;
+import com.aanglearning.principalapp.model.Message;
+import com.aanglearning.principalapp.util.EndlessRecyclerViewScrollListener;
 
-public class MessageActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MessageActivity extends AppCompatActivity implements MessageView{
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.progress)
+    ProgressBar progressBar;
+
+    private static final String TAG = "MessageActivity";
+    private MessagePresenter presenter;
+    private Groups group;
+    private ArrayList<Message> messages = new ArrayList<>();
+    private MessageAdapter adapter;
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        presenter = new MessagePresenterImpl(this, new MessageInteractorImpl());
+
+        setupRecyclerView();
+
+    }
+
+    private void setupRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        adapter = new MessageAdapter(this, messages);
+        recyclerView.setAdapter(adapter);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                presenter.getFollowupMessages(group.getId(), messages.get(messages.size()-1).getId());
+            }
+        };
+        recyclerView.addOnScrollListener(scrollListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.message_group_overflow, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.group_info:
+                //startActivity(new Intent(this, UserGroupActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        group = GroupDao.getGroup();
+        //groupName.setText(group.getName());
+        presenter.getMessages(group.getId());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        presenter.onDestroy();
+    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showError(String message) {
+        showSnackbar(message);
+    }
+
+    @Override
+    public void onMessageSaved(Message message) {
+        adapter.insertDataSet(message);
+    }
+
+    @Override
+    public void showMessages(ArrayList<Message> messages) {
+        this.messages = messages;
+        adapter.setDataSet(messages);
+    }
+
+    @Override
+    public void showFollowupMessages(ArrayList<Message> msgs) {
+        adapter.updateDataSet(msgs);
+        this.messages = adapter.getDataSet();
     }
 
 }
