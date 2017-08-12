13d77c14c49fd05b79ddc5e38c0bd2af98f38c4d
vinkrish <vinaykrishna1989@gmail.com>
2017-08-12 13:56:03 +0530

    PhotoView added for touch gesture on image.

--- app/src/main/java/com/aanglearning/principalapp/messagegroup/MessageViewActivity.java
+++ app/src/main/java/com/aanglearning/principalapp/messagegroup/MessageViewActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import com.aanglearning.principalapp.R;
 import com.aanglearning.principalapp.model.Message;
 import com.aanglearning.principalapp.util.SharedPreferenceUtil;
+import com.github.chrisbanes.photoview.PhotoView;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
@@ -25,7 +26,7 @@ public class MessageViewActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.shared_image)
-    ImageView sharedImage;
+    PhotoView sharedImage;
     @BindView(R.id.message)
     TextView messageTV;
 
