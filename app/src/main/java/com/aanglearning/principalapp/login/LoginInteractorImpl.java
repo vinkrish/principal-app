ba8a1411b9f4977dc8fefa4931dc79d86ae88361
vinkrish <vinaykrishna1989@gmail.com>
2017-08-18 11:16:53 +0530

    Release for sales

--- app/src/main/java/com/aanglearning/principalapp/login/LoginInteractorImpl.java
+++ app/src/main/java/com/aanglearning/principalapp/login/LoginInteractorImpl.java
@@ -32,7 +32,7 @@ class LoginInteractorImpl implements LoginInteractor {
                     SharedPreferenceUtil.saveAuthorizedUser(App.getInstance(), credentials.getUsername());
                     listener.onSuccess(response.body());
                 } else {
-                    listener.onError("Username and password don't match");
+                    listener.onError("mobile number and password don't match");
                 }
             }
 
