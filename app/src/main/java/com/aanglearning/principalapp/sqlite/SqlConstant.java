f5c1ca63e53dd8fe2b2713c845409362dcabbaea
vinkrish <vinaykrishna1989@gmail.com>
2017-10-14 14:34:11 +0530

    deleted_message table restructure

--- app/src/main/java/com/aanglearning/principalapp/sqlite/SqlConstant.java
+++ app/src/main/java/com/aanglearning/principalapp/sqlite/SqlConstant.java
@@ -6,7 +6,7 @@ package com.aanglearning.principalapp.sqlite;
 interface SqlConstant {
 
     String DATABASE_NAME = "principal.db";
-    int DATABASE_VERSION = 8;
+    int DATABASE_VERSION = 9;
 
     String CREATE_ATTENDANCE = "CREATE TABLE attendance (" +
             "  Id INTEGER PRIMARY KEY," +
@@ -160,6 +160,15 @@ interface SqlConstant {
             " ImageUrl TEXT, " +
             " VideoUrl TEXT, " +
             " CreatedAt TEXT" +
+            ")";
+
+    String CREATE_DELETED_MESSAGE = "CREATE TABLE deleted_message (" +
+            " Id INTEGER, " +
+            " MessageId INTEGER, " +
+            " SenderId INTEGER, " +
+            " RecipientId INTEGER, " +
+            " GroupId INTEGER, " +
+            " DeletedAt INTEGER" +
             ")";
 
     String CREATE_SERVICE = "CREATE TABLE service (" +
